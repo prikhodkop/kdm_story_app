90855baac9998deeed0369fb6b09341e8458e8f6
prikhodkop <prikhodkop@gmail.com>
2019-06-09 00:58:01 +0300

    another attempt on autoupdates

--- main.js
+++ main.js
@@ -1,11 +1,37 @@
 const {
 	app,
-	BrowserWindow
-} = require('electron')
+	BrowserWindow,
+	Menu,
+	protocol,
+	ipcMain
+} = require('electron');
+
+const {autoUpdater} = require("electron-updater");
 
 app.commandLine.appendSwitch('--autoplay-policy', 'no-user-gesture-required')
 
+let template = []
+if (process.platform === 'darwin') {
+  // OS X
+  const name = app.getName();
+  template.unshift({
+    label: name,
+    submenu: [
+      {
+        label: 'About ' + name,
+        role: 'about'
+      },
+      {
+        label: 'Quit',
+        accelerator: 'Command+Q',
+        click() { app.quit(); }
+      },
+    ]
+  })
+}
+
 function createWindow() {
+	autoUpdater.checkForUpdatesAndNotify();
 	// Create the browser window.
 	win = new BrowserWindow({
 		// width: 1440,
@@ -30,6 +56,51 @@ function createWindow() {
 
 }
 
+let win;
 
+function sendStatusToWindow(text) {
+  log.info(text);
+  win.webContents.send('message', text);
+}
+// function createDefaultWindow() {
+//   win = new BrowserWindow();
+//   win.webContents.openDevTools();
+//   win.on('closed', () => {
+//     win = null;
+//   });
+//   win.loadURL(`file://${__dirname}/version.html#v${app.getVersion()}`);
+//   return win;
+// }
+autoUpdater.on('checking-for-update', () => {
+  sendStatusToWindow('Checking for update...');
+})
+autoUpdater.on('update-available', (info) => {
+  sendStatusToWindow('Update available.');
+})
+autoUpdater.on('update-not-available', (info) => {
+  sendStatusToWindow('Update not available.');
+})
+autoUpdater.on('error', (err) => {
+  sendStatusToWindow('Error in auto-updater. ' + err);
+})
+autoUpdater.on('download-progress', (progressObj) => {
+  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  sendStatusToWindow(log_message);
+})
+autoUpdater.on('update-downloaded', (info) => {
+  sendStatusToWindow('Update downloaded');
+});
+app.on('ready', function() {
+  // Create the Menu
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
 
-app.on('ready', createWindow)
+  createWindow();
+});
+app.on('window-all-closed', () => {
+  app.quit();
+});
+
+// app.on('ready', createWindow)
