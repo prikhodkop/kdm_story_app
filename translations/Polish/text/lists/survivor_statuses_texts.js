2ca45df322f540922a2bebbe673128d66ea5cf6c
Sir_Yaro <jaroslaw@czarniak.org>
2020-02-17 09:19:30 +0100

    gear, resources statuses PL (#50)
    * typo fix
    
    * gear, resources statuses PL
    
    Co-authored-by: Pavel Prikhodko <prikhodkop@gmail.com>

---
+++ translations/Polish/text/lists/survivor_statuses_texts.js
@@ -0,0 +1,86 @@
+const texts = {
+ 'Battle Tempo': {
+   'label': 'Tempo bitwy',
+   'description': '#'
+ },
+ 'Black Pond': {
+   'label': 'Black Pond',
+   'description': '#'
+ },
+ 'Darkness Awareness': {
+   'label': 'Darkness Awareness',
+   'description': '#'
+ },
+ 'Dreaded Decade': {
+   'label': 'Dekada Lęku',
+   'description': '#'
+ },
+ 'Dung Milk': {
+   'label': 'Dung Milk',
+   'description': '#'
+ },
+ 'Flower Funnel': {
+   'label': 'Flower Funnel',
+   'description': '#'
+ },
+ 'Gobbled Up': {
+   'label': 'Połknięcie',
+   'description': '#'
+ },
+ 'Horripilation': {
+   'label': 'Okropność',
+   'description': '#'
+ },
+ 'Impaled': {
+   'label': 'Impaled',
+   'description': '#'
+ },
+ 'Infectious Lunacy': {
+   'label': 'Zaraźliwy obłęd',
+   'description': '#'
+ },
+ 'Madness Inversion': {
+   'label': 'Madness Inversion',
+   'description': '#'
+ },
+ 'Momentum': {
+   'label': 'Momentum',
+   'description': '#'
+ },
+ 'Pod Imprisonment': {
+   'label': 'Pod Imprisonment',
+   'description': '#'
+ },
+ 'Polarized Aura': {
+   'label': 'Spolaryzowana Aura',
+   'description': '#'
+ },
+ 'Retinue': {
+   'label': 'Oddział',
+   'description': '#'
+ },
+ 'Secondary Forge': {
+   'label': 'Druga Kuźnia',
+   'description': '#'
+ },
+ 'Smell World': {
+   'label': 'Smell World',
+   'description': '#'
+ },
+ 'Starward': {
+   'label': 'Starward',
+   'description': '#'
+ },
+ 'Steadfast': {
+   'label': 'Steadfast',
+   'description': '#'
+ },
+ 'Stolen Dreams': {
+   'label': 'Skradzione sny',
+   'description': '#'
+ },
+}
+
+module.exports = {
+ texts,
+}
