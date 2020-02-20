43e0d35d627b403c6eb1ad583f1cd4263d7683fa
SirYaro <jaroslaw@czarniak.org>
2020-02-20 11:51:14 +0100

    s_events passives PL

--- versions/1.5 (Polish)/text/lists/settlement_events_texts.js
+++ versions/1.5 (Polish)/text/lists/settlement_events_texts.js
@@ -1,81 +1,121 @@
 const texts = {
  'Acid Storm': {
-   'label': 'Kwasowa burza',
+   'label':'Kwasowa burza',
+   'passive':{
+     'settlement': [
+       '<b class="event-text">[Wydarzenie]</b> Burza wstrzymuje wykonywanie działań w <b class="music-text">muzyce</b>.',
+       '<b class="event-text">[Wydarzenie]</b>  Burza inspiruje. Dodaj +2 do wyniku rzutów działań w <b class="art-text">sztuce</b>.'
+     ],
+   },
  },
  'Clinging Mist': {
-   'label': 'Gęsta mgła',
+   'label':'Gęsta mgła',
  },
  'Cracks in the Ground': {
-   'label': 'Pęknięcia w ziemi',
+   'label':'Pęknięcia w ziemi',
+   'passive':{
+     'settlement': [
+       '<b class="event-text">[Wydarzenie]</b> Pęknięcia wstrzymują wykonywanie działań we wszystkich <b class="home-text">domach</b>.',
+       '<b class="event-text">[Wydarzenie]</b>  Opary poprawiają oddychanie! +1 do wyniku rzutu działań, podczas <b>koszmarnego treningu</b>.'
+     ]
+   },
  },
  'Dark Dentist': {
-   'label': 'Mroczny dentysta',
+   'label':'Mroczny dentysta',
+   'action': true,
  },
  'Dark Trader': {
-   'label': 'Mroczny handlarz',
+   'label':'Mroczny handlarz',
+   'action': true,
  },
  'Elder Council': {
-   'label': 'Rada starszych',
+   'label':'Rada starszych',
  },
  'First Day': {
-   'label': 'Dzień pierwszy',
+   'label':'Dzień pierwszy',
  },
  'Glossolalia': {
-   'label': 'Glosolalia',
+   'label':'Glosolalia',
+   'passive':{
+     'settlement': [
+       '<b class="event-text">[Wydarzenie]</b> Glosolalia uniemożliwia wykonywanie w tym roku działań <b class="science-text">nauki</b> i <b class="education-text">edukacji</b>.',
+     ]
+   },
  },
  'Gorm Climate': {
-   'label': 'Klimat Gorma',
+   'label':'Klimat Gorma',
  },
  'Haunted': {
-   'label': 'Nawiedzenie',
+   'label':'Nawiedzenie',
  },
  'Heat Wave': {
-   'label': 'Fala upałów',
- },
+   'label':'Fala upałów',
+   'passive':{
+     'settlement': [
+       '<b class="event-text">[Wydarzenie]</b> Fala upałów uniemożliwia wykonywanie w tym roku działań <b class="science-text">nauki</b> i <b class="education-text">edukacji</b>.',
+       '<b class="event-text">[Wydarzenie]</b> Jeśli <b>wyruszasz</b> z ciężkim ekwipunkiem w siatce, po zastosowaniu wszystkich bonusów ustaw swoje przetrwanie na 0.',
+     ]
+   },
+},
  'Hunt Reenactment': {
-   'label': 'Rekonstrukcja łowów',
+   'label':'Rekonstrukcja łowów',
  },
  'Lights in the Sky': {
-   'label': 'Światła na niebie',
+   'label':'Światła na niebie',
+   'passive':{
+     'settlement': [
+       '<b class="event-text">[Wydarzenie]</b> Po rzucie na działania, możesz dodać +1 lub +2 do wyniku.',
+     ]
+   },
+
  },
  'Murder': {
-   'label': 'Morderstwo',
+   'label':'Morderstwo',
  },
  'Nickname': {
-   'label': 'Przydomek',
+   'label':'Przydomek',
  },
  'Open Maw': {
-   'label': 'Otwarta paszcza',
+   'label':'Otwarta paszcza',
  },
  'Phantom': {
-   'label': 'Phantom',
+   'label':'',
+  'expansion': 'slenderman',
  },
  'Plague': {
-   'label': 'Zaraz',
+   'label':'Zaraza',
  },
  'Rivalry': {
-   'label': 'Rywalizacja',
+   'label':'Rywalizacja',
  },
  'Season of the Spiderling': {
-  'label': 'Sezon na Pajęczątka',
+   'label':'Sezon na Pajęczątka',
  },
  'Silk Storm': {
-  'label': 'Jedwabna burza',
+   'label':'Jedwabna burza',
+  'passive':{
+    'settlement': [
+      '<b class="event-text">[Wydarzenie]</b> Zdobywacie +2 działania podczas tej fazy osady - mogą zostać wydane jedynie na innowacje Pajęczydła.',
+      '<b class="event-text">[Wydarzenie]</b> Ocalali nie mogą <b>wyruszyć</b> w tym roku o ile nie mają 1 wolnego pola w swojej siatce ekwipunku.',
+    ]
+  },
+
  },
  'Skull Eater': {
-   'label': 'Zjadacz czaszek',
+   'label':'Zjadacz czaszek',
  },
  'Slender Blight': {
-  'label': 'Slender Blight',
+   'label':'',
+  'expansion': 'slenderman',
  },
  'Stranger in the Dark': {
-   'label': 'Nieznajomy w ciemności',
+   'label':'Nieznajomy w ciemności',
  },
  'Triathlon of Death': {
-   'label': 'Triathlon śmierci',
+   'label':'Triathlon śmierci',
  },
  'Weird Dream': {
-   'label': 'Dziwny sen',
+   'label':'Dziwny sen',
  },
 }
 
