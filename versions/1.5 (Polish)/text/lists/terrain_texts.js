da3480a1e30e50ab7f28481b80dfe4b08e2059a1
SirYaro <jaroslaw@czarniak.org>
2020-02-21 23:16:09 +0100

    terrains PL

---
+++ versions/1.5 (Polish)/text/lists/terrain_texts.js
@@ -0,0 +1,92 @@
+const texts = {
+ '2 Acantus Plants': {
+  'label':'2 rośliny ciemiernika',
+  'description': '#',
+  'num_cards': 4
+ },
+ '2 Salt Sculptures': {
+   'label':'',
+   'description': '#',
+  'expansion': 'sunstalker'
+ },
+ '2 Tall Grass': {
+   'label':'2 wysokie trawy',
+   'description': '#',
+  'num_cards': 2
+ },
+ '3 Stone Columns': {
+   'label':'3 kamienne kolumny',
+   'description': '#',
+  'num_cards': 2
+ },
+ 'Bug Patch': {
+   'label':'Kłębowisko much',
+   'description': '#',
+ },
+ 'Dead Monster': {
+   'label':'Martwy potwór',
+   'description': '#',
+ },
+ 'Debris': {
+   'label':'Szczątki',
+   'description': '#',
+ },
+ 'Giant Stone Face': {
+   'label':'Gigantyczna kamienna twarz',
+   'description': '#',
+  'num_cards': 2
+ },
+ 'Lava Pool': {
+   'label':'',
+   'description': '#',
+  'expansion': 'dragon king'
+ },
+ 'Lion Statue': {
+   'label':'',
+   'description': '#',
+  'expansion': 'lion god'
+ },
+ 'Nightmare Tree': {
+   'label':'Drzewo koszmarów',
+   'description': '#',
+ },
+ 'Obsidian Tower': {
+   'label':'',
+   'description': '#',
+  'expansion': 'dragon king'
+ },
+ 'Ore Vein': {
+   'label':'Żyła rudy',
+   'description': '#',
+  'num_cards': 2
+ },
+ 'Resin Dung Ball': {
+   'label':'',
+   'description': '#',
+  'expansion': 'dung beetle knight'
+ },
+ 'Sinkhole': {
+   'label':'',
+   'description': '#',
+  'expansion': 'lion god'
+ },
+ 'Survivor Corpse': {
+   'label':'Zwłoki Ocalałego',
+   'description': '#',
+  'num_cards': 2
+ },
+ 'The Lonely Tree': {
+   'label':'',
+   'description': '#',
+  'expansion': 'lonely tree'
+ },
+ 'Toppled Pillar': {
+   'label':'Przewrócony filar',
+   'description': '#',
+  'num_cards': 2
+ },
+}
+
+module.exports = {
+ texts,
+}
