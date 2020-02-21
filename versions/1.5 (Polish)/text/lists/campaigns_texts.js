d73dec0a25479d7a525abd6bfbf27711a3269bcf
SirYaro <jaroslaw@czarniak.org>
2020-02-21 23:27:11 +0100

    campaign txt PL

---
+++ versions/1.5 (Polish)/text/lists/campaigns_texts.js
@@ -0,0 +1,22 @@
+const texts = {
+ 'Lantern': {
+   'start_event': 'first story',
+   'label': 'Lud Latarni',
+   'description': 'Rdzeniem, KDM jest opowieść o kruchości ludzkiego życia, dzikości ludzkiego ducha i cudach eksploracji.</br></br>Twoja historia będzie kształtowana przez decyzje, które podejmujesz, atuty i słabości Twojej osady, i rozwój Twoich Ocalałych.',
+ },
+ 'Sun': {
+   'start_event': 'first story',
+   'label': 'People of the Sun',
+   'description': 'Once sky had no day. There was a Shadow that was sad, it could only play in small light of a lantern. The Shadow found a hole in the sky and decided to steal enough lanterns to fill it. It took many generations, and when it was done the shadow slept under the big light, dreaming dreams about how it could play. When it awoke, all of the light was gone! At the center of the hole in the sky, was a tiny shining entity, with a great big mouth. The shadow named the tiny thing the sun and they played together.',
+ },
+ 'Stars': {
+   'start_event': 'first story',
+   'label': 'People of the Stars',
+   'description': 'Adopted by the Tyrant, the last of the People of the Stars, the survivors must grow their settlement to please its desire to leave an enduring draconian legacy before he passes.',
+ },
+
+}
+
+module.exports = {
+ texts,
+}
