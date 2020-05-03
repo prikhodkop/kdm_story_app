ce67396e38bdc0986bdba072c573dac692d6e538
Pavel Prikhodko <prikhodkop@gmail.com>
2020-05-03 03:08:32 +0300

    fixed text typo for love juice desc

--- versions/1.5/text/lists/resources_texts.js
+++ versions/1.5/text/lists/resources_texts.js
@@ -103,7 +103,7 @@ const texts = {
 
 'qqq': {'label': '???', 'group_name':'Basic', 'image': 'true', 'type': 'organ/bone/hide/consumable', 'text': '<i>You have no idea what monster bit this is.</i><br /><br />Can be used as a bone, organ, or hide!<br /><br />Drop chance: 2/21.'},
 'Broken Lantern': {'image': 'true', 'group_name':'Basic', 'type': 'scrap', 'text': '<i>Remains of an extinguished lantern.</i><br /><br />Drop chance: 2/21.'},
-'Love Juice': {'image': 'true', 'group_name':'Basic', 'type': 'organ/consumable', 'text': 'During the settlement phase, you may archive this to triggrer <b>Intimacy</b> story event. Nominated survivors must be able to consume.<br /><br />Drop chance: 2/21.'},
+'Love Juice': {'image': 'true', 'group_name':'Basic', 'type': 'organ/consumable', 'text': 'During the settlement phase, you may archive this to trigger <b>Intimacy</b> story event. Nominated survivors must be able to consume.<br /><br />Drop chance: 2/21.'},
 'Monster Bone': {'image': 'true', 'group_name':'Basic', 'type': 'bone', 'text': '<i>A bone suitable for crafting.</i><br /><br />Drop chance: 4/21.'},
 'Monster Hide': {'image': 'true', 'group_name':'Basic', 'type': 'hide', 'text': '<i>The skin of a beast.</i><br /><br />Drop chance: 7/21.'},
 'Monster Organ': {'image': 'true', 'group_name':'Basic', 'type': 'organ/consumable', 'text': 'If you <b>consume</b> this, archive this card. Roll 1d10:<br /><b style=\"color:#cc0;\">6+</b><div style="font-size:0.9em;display: inline;"> - you contract a parasite. Archive all consumable gear in your grid now.</div><br /><br />Drop chance: 3/21.'},
