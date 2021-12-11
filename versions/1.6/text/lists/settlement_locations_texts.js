05c6b479b6d82ff9fea5a6defcb0c1655f12ae6d
Jams <irwin.james.d@gmail.com>
2021-12-11 17:54:36 -0500

    Add 1.6 version and some assets

--- versions/1.6/text/lists/settlement_locations_texts.js
+++ versions/1.6/text/lists/settlement_locations_texts.js
@@ -2,8 +2,8 @@ const texts = {
  'Barber Surgeon': {
   'label':'',
   'gear': {
-   '1': ['First Aid Kit', 'Brain Mint', 'Elder Earrings', 'Musk Bomb'],
-   '2': ['Scavenger Kit', 'Bug Trap', 'Speed Powder', 'Almanac'],
+   '1': ['First Aid Kit', 'Almanac', 'Musk Bomb', 'Red Charm'],
+   '2': ['Scavenger Kit', 'Bug Trap', 'Blue Charm', 'Green Charm'],
    '3': []},
   'action': true,
  },
@@ -11,8 +11,8 @@ const texts = {
    'label':'',
   'gear': {
    '1': ['Lantern Helm', 'Lantern Gauntlets', 'Lantern Cuirass', 'Lantern Mail', 'Lantern Greaves'],
-   '2': ['Lantern Sword', 'Lantern Dagger', 'Lantern Glaive', 'Scrap Shield'],
-   '3': ['Beacon Shield', 'Ring Whip', 'Dragon Slayer', 'Perfect Slayer']},
+   '2': ['Lantern Sword', 'Lantern Dagger', 'Lantern Glaive', 'Dragon Slayer','Perfect Slayer'],
+   '3': ['Scrap Shield', 'Beacon Shield', 'Ring Whip', 'Polishing Lantern']},
   'armor_set': 'Lantern Armor',
  },
  'Bone Smith': {
@@ -128,17 +128,14 @@ const texts = {
    '2': ['Dried Acantus', 'Lucky Charm', 'Monster Tooth Necklace'],
    '3': []},
   'action': true,
-  'num_actions': 2,
-  'action_disabler': {
-    1: {'location': ['Stone Circle']},
-  }
+  'num_actions': 1,
  },
  'Plumery': {
    'label':'',
   'gear': {
-   '1': ['Phoenix Helm', 'Phoenix Gauntlet', 'Phoenix Plackart', 'Phoenix Faulds', 'Phoenix Greaves'],
-   '2': ['Feather Shield', 'Bloom Sphere', 'Sonic Tomahawk', 'Hollow Sword', 'Arc Bow'],
-   '3': ['Feather Mantle', 'Bird Bread', 'Hours Ring', 'Crest Crown', 'Hollowpoint Arrow']},
+   '1': ['Phoenix Helm', 'Phoenix Gauntlets', 'Phoenix Plackart', 'Phoenix Faulds', 'Phoenix Greaves'],
+   '2': ['Feather Shield', 'Bloom Sphere', 'Sonic Tomahawk', 'Hollow Sword', 'Arc Bow', 'Finger of God', 'Rainbow Katana'],
+   '3': ['Feather Mantle', 'Bird Bread', 'Hours Ring', 'Crest Crown', 'Hollowpoint Arrow', 'Blood Sheath']},
   'armor_set': 'Phoenix Armor',
  },
  'Sacred Pool': {
@@ -183,8 +180,8 @@ const texts = {
    'label':'',
   'gear': {
    '1': ['Screaming Horns', 'Screaming Bracers', 'Screaming Coat', 'Screaming Skirt', 'Screaming Leg Warmers'],
-   '2': ['Boss Mehndi', 'Beast Knuckle', 'Red Charm', 'Blood Paint'],
-   '3': ['Bone Earrings', 'Green Charm', 'Blue Charm', 'Lance of Longinus']},
+   '2': ['Boss Mehndi', 'Beast Knuckle', 'Brain Mint', 'Blood Paint'],
+   '3': ['Bone Earrings', 'Speed Powder', 'Elder Earrings', 'Lance of Longinus']},
   'action': true,
   'armor_set': 'Screaming Armor',
   'action_enabler': {'innovation': ['Forbidden Dance']},
@@ -222,8 +219,8 @@ const texts = {
  'Weapon Crafter': {
    'label':'',
   'gear': {
-   '1': ['Counterweighted Axe', 'Whistling Mace', 'Zanbato', 'Blood Sheath', 'Rainbow Katana'],
-   '2': ['Skullcap Hammer', 'Scrap Sword', 'Scrap Dagger', 'Finger of God'],
+   '1': ['Counterweighted Axe', 'Whistling Mace', 'Scrap Lantern', 'Scrap Rebar', 'Zanbato'],
+   '2': ['Skullcap Hammer', 'Scrap Sword', 'Scrap Dagger', 'Scrap Bone Spear'],
    '3': []},
   'action': true,
   'num_actions': 2,
